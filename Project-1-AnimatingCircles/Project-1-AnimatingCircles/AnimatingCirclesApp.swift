2b9db4c0e65cf68af290dc9836fe27e729eb3cd7
Michael Pascucci <mdpascucci@live.com>
2022-07-17 09:20:03 -0400

    Complete Project 1 - Animating Circles

---
+++ Project-1-AnimatingCircles/Project-1-AnimatingCircles/AnimatingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_AnimatingCirclesApp.swift
+//  Project-1-AnimatingCircles
+//
+//  Created by Michael & Diana Pascucci on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project-1-AnimatingCircles/Project-1-AnimatingCircles/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  Project-1-AnimatingCircles
+//
+//  Created by Michael & Diana Pascucci on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    @State private var scaleInOut = false
+    @State private var rotateInOut = false
+    @State private var moveInOut = false
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack {
+            // Background
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+            
+            // Foreground
+            ZStack {
+                
+                // MARK: - CIRCLE SET 1
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .top,
+                                             endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .bottom,
+                                             endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }
+                .opacity(0.5)
+                
+                // MARK: - CIRCLE SET 2
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .top,
+                                             endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .bottom,
+                                             endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }
+                .opacity(0.5)
+                .rotationEffect(.degrees(60))
+                
+                // MARK: - CIRCLE SET 3
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .top,
+                                             endPoint: .bottom))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.green, .white]),
+                                             startPoint: .bottom,
+                                             endPoint: .top))
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
+                }
+                .opacity(0.5)
+                .rotationEffect(.degrees(120))
+                
+            }
+            .rotationEffect(.degrees(rotateInOut ? 90 : 0))
+            .scaleEffect(scaleInOut ? 1 : 1/4)
+            .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8), value: moveInOut)
+            .onAppear {
+                moveInOut.toggle()
+                scaleInOut.toggle()
+                rotateInOut.toggle()
+            }
+        }
+    }
+}
+
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
